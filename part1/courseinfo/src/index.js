74217ea6cd89d81bbba989eef1193839fb4ec782
Maaria Wahlström <65168298+maariaw@users.noreply.github.com>
2020-09-02 11:01:38 +0300

    Complete steps 1 and 2 of courseinfo excercises in part 1

---
+++ part1/courseinfo/src/index.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => (
+  <>
+    <h1>{props.title}</h1>
+  </>
+)
+
+const Part = (props) => (
+  <>
+    <p>{props.name} {props.exercises}</p>
+  </>
+)
+
+const Content = (props) => (
+  <>
+    <Part name={props.part1} exercises={props.exercises1} />
+    <Part name={props.part2} exercises={props.exercises2} />
+    <Part name={props.part3} exercises={props.exercises3} />
+  </>
+)
+
+const Total = (props) => (
+  <>
+    <p>Number of exercises {props.total}</p>
+  </>
+)
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header title={course} />
+      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Total total={exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
