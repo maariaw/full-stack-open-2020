ba5f0beee7f4fb6e57f0d2a9c88a74c712fcd51e
Maaria Wahlström <65168298+maariaw@users.noreply.github.com>
2021-05-25 13:22:37 +0300

    Add tests for blog api

---
+++ part4/tests/blog_api.test.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+const helper = require('./test_helper')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(helper.initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(helper.initialBlogs[1])
+  await blogObject.save()
+  blogObject = new Blog(helper.initialBlogs[2])
+  await blogObject.save()
+})
+
+test('correct amount of blog posts returned as json', async () => {
+  const response = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
