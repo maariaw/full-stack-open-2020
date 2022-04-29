16870f82c379adf903f782c75e5f443a951ff37d
Maaria Wahlström <65168298+maariaw@users.noreply.github.com>
2022-04-29 15:04:03 +0300

    Implement button to show and hide details of a blog (ex 5.7, 5.6 already done)

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,46 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({blog}) => {
+  const [viewDetails, setViewDetails] = useState(false)
+
+  const toggleDetail = () => {
+    setViewDetails(!viewDetails)
+  }
+
+  const blogStyle = {
+    paddingTop: 5,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const buttonStyle = {
+    marginLeft: 10
+  }
+
+  if (!viewDetails) {
+    return (
+      <div style={blogStyle}>
+      <p>
+      <b>{blog.title}</b> by {blog.author}
+      <button onClick={toggleDetail} style={buttonStyle}>View</button>
+      </p>
+    </div>
+    )
+  }
+
+  return (
+    <div style={blogStyle}>
+      <p>
+        <b>{blog.title}</b> by {blog.author}
+      <button onClick={toggleDetail} style={buttonStyle}>Hide</button>
+      </p>
+      <p>{blog.url}</p>
+      <p>{blog.likes} <button style={buttonStyle}>Like</button></p>
+      <p>{blog.user.name}</p>
+    </div>
+  )
+}
 
 export default Blog
