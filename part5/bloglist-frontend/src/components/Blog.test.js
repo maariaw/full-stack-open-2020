976c066b4ceedf9b55759f1fd64c3d50d77f8873
Maaria Wahlström <65168298+maariaw@users.noreply.github.com>
2022-05-03 13:12:50 +0300

    Test that pressing View shows blog url and likes (ex 5.14)

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,36 +1,59 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('renders author and title, not url or likes', () => {
-  const blog = {
-    author: 'T. Ester',
-    id: '626a6a39e3f66637e0140cd2',
-    likes:'3',
-    title:'Only title and author should be rendered',
-    url:'this is a website',
-    user:{
-      id: '62645ce7f97fb0021797e689',
-      name: 'Kalle Kupari',
-      username: 'copper'
+describe('<Blog />', () => {
+  let blog
+
+  beforeEach(() => {
+    blog = {
+      author: 'T. Ester',
+      id: '626a6a39e3f66637e0140cd2',
+      likes:'3',
+      title:'This is a test blog',
+      url:'this is a website',
+      user:{
+        id: '62645ce7f97fb0021797e689',
+        name: 'Kalle Kupari',
+        username: 'copper'
+      }
     }
-  }
+  })
 
-  render(<Blog
-    blog={blog}
-    isCreator={true}
-  />)
+  test('renders author and title, not url or likes', () => {
+    render(<Blog
+      blog={blog}
+      isCreator={true}
+    />)
 
-  const titleElement = screen.getByText(
-    'Only title and author should be rendered',
-    { exact: false }
-  )
-  expect(titleElement).toBeDefined()
-  const authorElement = screen.getByText('T. Ester', { exact: false })
-  expect(authorElement).toBeDefined()
-  const urlElement = screen.queryByText('this is a website')
-  expect(urlElement).toBeNull()
-  const likesEelement = screen.queryByTestId('likes')
-  expect(likesEelement).toBeNull()
+    const titleElement = screen.getByText(
+      'This is a test blog',
+      { exact: false }
+    )
+    expect(titleElement).toBeDefined()
+    const authorElement = screen.getByText('T. Ester', { exact: false })
+    expect(authorElement).toBeDefined()
+    const urlElement = screen.queryByText('this is a website')
+    expect(urlElement).toBeNull()
+    const likesEelement = screen.queryByTestId('likes')
+    expect(likesEelement).toBeNull()
+  })
+
+  test('renders url and likes after pressing "View"', async () => {
+    render(<Blog
+      blog={blog}
+      isCreator={true}
+    />)
+
+    const user = userEvent.setup()
+    const button = screen.getByText('View')
+    await user.click(button)
+
+    const urlElement = screen.queryByText('this is a website')
+    expect(urlElement).not.toBeNull()
+    const likesElement = screen.queryByTestId('likes')
+    expect(likesElement).not.toBeNull()
+  })
 })
