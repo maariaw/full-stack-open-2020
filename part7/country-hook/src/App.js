760d9b54f0a5e2f1f430526fcaf4551c6f16ce1e
Maaria Wahlström <65168298+maariaw@users.noreply.github.com>
2022-05-13 14:31:52 +0300

    Implement fetching country details (ex 7.7)

--- part7/country-hook/src/App.js
+++ part7/country-hook/src/App.js
@@ -18,8 +18,18 @@ const useField = (type) => {
 const useCountry = (name) => {
   const [country, setCountry] = useState(null)
 
-  useEffect(() => {})
-
+  useEffect(() => {
+    if (name) {
+      axios
+      .get(`https://restcountries.com/v3.1/name/${name}?fullText=true`)
+      .then(response => {
+        setCountry(response.data[0])
+      })
+      .catch(error => {
+        setCountry(null)
+      })
+    }
+  }, [name])
   return country
 }
 
