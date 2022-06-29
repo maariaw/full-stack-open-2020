bff254bfb428d30cfc6e9293ed234fdd022fabae
Maaria Wahlström <65168298+maariaw@users.noreply.github.com>
2022-06-29 16:31:25 +0300

    Refactor showing blogs into their own component Blogs

--- part7/bloglist-frontend/src/App.js
+++ part7/bloglist-frontend/src/App.js
@@ -1,34 +1,20 @@
-import { useEffect, useRef } from 'react'
-import Blog from './components/Blog'
+import { useEffect } from 'react'
+import Blogs from './components/Blogs'
 import Notification from './components/Notification'
-import NewBlogForm from './components/NewBlogForm'
 import blogService from './services/blogs'
 import loginService from './services/login'
-import Togglable from './components/Togglable'
 import {
   setNotification,
   nullNotification,
 } from './reducers/notificationReducer'
-import {
-  initializeBlogs,
-  likeBlog,
-  deleteBlog,
-  createBlog,
-} from './reducers/blogReducer'
 import { setUser } from './reducers/userReducer'
 import { useSelector, useDispatch } from 'react-redux'
 
 const App = () => {
-  const blogFormRef = useRef()
-
   const dispatch = useDispatch()
   const notification = useSelector((state) => state.notification)
   const blogs = useSelector((state) => state.blogs)
   const user = useSelector((state) => state.user)
-
-  useEffect(() => {
-    dispatch(initializeBlogs())
-  }, [dispatch])
 
   useEffect(() => {
     const loggedUserJSON = window.localStorage.getItem('loggedBlogsUser')
@@ -70,32 +56,6 @@ const App = () => {
     }, 5000)
   }
 
-  const addBlog = async (blogObject) => {
-    try {
-      dispatch(createBlog(blogObject, user))
-      blogFormRef.current.toggleVisibility()
-      dispatch(
-        setNotification(
-          `Added new blog: "${blogObject.title}" by ${blogObject.author}`
-        )
-      )
-      setTimeout(() => {
-        dispatch(nullNotification())
-      }, 5000)
-    } catch (exception) {
-      dispatch(setNotification('Error: Failed to add blog'))
-      setTimeout(() => {
-        dispatch(nullNotification())
-      }, 5000)
-    }
-  }
-
-  const newBlogForm = () => (
-    <Togglable buttonLabel='Add new blog' ref={blogFormRef}>
-      <NewBlogForm addNewBlog={addBlog} />
-    </Togglable>
-  )
-
   const loginForm = () => (
     <form onSubmit={handleLogin}>
       <div>
@@ -116,41 +76,6 @@ const App = () => {
     </form>
   )
 
-  const giveLike = async (id) => {
-    try {
-      dispatch(likeBlog(id))
-    } catch (exception) {
-      dispatch(setNotification('Error: Failed to add like'))
-      setTimeout(() => {
-        dispatch(nullNotification())
-      }, 5000)
-    }
-  }
-
-  const handleRemove = async (id) => {
-    const blogToDelete = blogs.find((b) => b.id === id)
-    const confirmText = `Are you sure you want to remove this blog:
-    "${blogToDelete.title}" by ${blogToDelete.author}`
-    if (window.confirm(confirmText)) {
-      try {
-        dispatch(deleteBlog(id))
-        dispatch(
-          setNotification(
-            `Removed blog "${blogToDelete.title}" by ${blogToDelete.author}`
-          )
-        )
-        setTimeout(() => {
-          dispatch(nullNotification())
-        }, 5000)
-      } catch (exception) {
-        dispatch(setNotification('Error: Failed to remove blog'))
-        setTimeout(() => {
-          dispatch(nullNotification())
-        }, 5000)
-      }
-    }
-  }
-
   if (user === null) {
     return (
       <div>
@@ -169,19 +94,7 @@ const App = () => {
       <button onClick={handleLogout} data-cy='logout'>
         Logout
       </button>
-      {newBlogForm()}
-      <br />
-      <div>
-        {blogs.map((blog) => (
-          <Blog
-            key={blog.id}
-            blog={blog}
-            handleLike={() => giveLike(blog.id)}
-            isCreator={user.username === blog.user.username}
-            handleRemove={() => handleRemove(blog.id)}
-          />
-        ))}
-      </div>
+      <Blogs />
     </div>
   )
 }
---
+++ part7/bloglist-frontend/src/components/Blogs.js
@@ -0,0 +1,107 @@
+import { useEffect, useRef } from 'react'
+import NewBlogForm from './NewBlogForm'
+import Togglable from './Togglable'
+import Blog from './Blog'
+import { useSelector, useDispatch } from 'react-redux'
+import {
+  initializeBlogs,
+  likeBlog,
+  deleteBlog,
+  createBlog,
+} from '../reducers/blogReducer'
+import {
+  setNotification,
+  nullNotification,
+} from '../reducers/notificationReducer'
+
+const Blogs = () => {
+  const blogFormRef = useRef()
+  const dispatch = useDispatch()
+  const blogs = useSelector((state) => state.blogs)
+  const user = useSelector((state) => state.user)
+
+  useEffect(() => {
+    dispatch(initializeBlogs())
+  }, [dispatch])
+
+  const addBlog = async (blogObject) => {
+    try {
+      dispatch(createBlog(blogObject, user))
+      blogFormRef.current.toggleVisibility()
+      dispatch(
+        setNotification(
+          `Added new blog: "${blogObject.title}" by ${blogObject.author}`
+        )
+      )
+      setTimeout(() => {
+        dispatch(nullNotification())
+      }, 5000)
+    } catch (exception) {
+      dispatch(setNotification('Error: Failed to add blog'))
+      setTimeout(() => {
+        dispatch(nullNotification())
+      }, 5000)
+    }
+  }
+
+  const giveLike = async (id) => {
+    try {
+      dispatch(likeBlog(id))
+    } catch (exception) {
+      dispatch(setNotification('Error: Failed to add like'))
+      setTimeout(() => {
+        dispatch(nullNotification())
+      }, 5000)
+    }
+  }
+
+  const handleRemove = async (id) => {
+    const blogToDelete = blogs.find((b) => b.id === id)
+    const confirmText = `Are you sure you want to remove this blog:
+    "${blogToDelete.title}" by ${blogToDelete.author}`
+    if (window.confirm(confirmText)) {
+      try {
+        dispatch(deleteBlog(id))
+        dispatch(
+          setNotification(
+            `Removed blog "${blogToDelete.title}" by ${blogToDelete.author}`
+          )
+        )
+        setTimeout(() => {
+          dispatch(nullNotification())
+        }, 5000)
+      } catch (exception) {
+        dispatch(setNotification('Error: Failed to remove blog'))
+        setTimeout(() => {
+          dispatch(nullNotification())
+        }, 5000)
+      }
+    }
+  }
+
+  const newBlogForm = () => (
+    <Togglable buttonLabel='Add new blog' ref={blogFormRef}>
+      <NewBlogForm addNewBlog={addBlog} />
+    </Togglable>
+  )
+
+  return (
+    <div>
+      {newBlogForm()}
+      <br />
+      <div>
+        {blogs.map((blog) => (
+          <Blog
+            key={blog.id}
+            blog={blog}
+            handleLike={() => giveLike(blog.id)}
+            isCreator={user.username === blog.user.username}
+            handleRemove={() => handleRemove(blog.id)}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Blogs
